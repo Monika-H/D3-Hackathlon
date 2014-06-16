cabd7818d6e280f7ed6dbad4813c5122b6196c3e
Volker Schnecke <volker.schnecke@bredband.net>
2014-06-16 16:26:57 +0200

    Conflict Resolution

--- js/drawBoxPlot.js
+++ js/drawBoxPlot.js
@@ -5,12 +5,12 @@ drawBoxPlot = function(data,svg) {
 
 		for (var index = 0; index < data[keys[1]].length; index++) {
 			object = {
-				visit: d3.max(data.map(function(d){ return d[index][x_var];})),
-				maximum: d3.max(data.map(function(d){ return d[index][y_var];})),
-				minimum: d3.min(data.map(function(d){ return d[index][y_var];})),
-				median: d3.median(data.map(function(d){ return d[index][y_var];})),
-				quartile_1: d3.quantile(d3.values(data.map(function(d){ return d[index][y_var];})).sort(d3.ascending),0.25),
-				quartile_3: d3.quantile(d3.values(data.map(function(d){ return d[index][y_var];})).sort(d3.ascending),0.75)
+				visit: d3.max(data.map(function(d){ return d[index][xVar];})),
+				maximum: d3.max(data.map(function(d){ return d[index][yVar];})),
+				minimum: d3.min(data.map(function(d){ return d[index][yVar];})),
+				median: d3.median(data.map(function(d){ return d[index][yVar];})),
+				quartile_1: d3.quantile(d3.values(data.map(function(d){ return d[index][yVar];})).sort(d3.ascending),0.25),
+				quartile_3: d3.quantile(d3.values(data.map(function(d){ return d[index][yVar];})).sort(d3.ascending),0.75)
 				}
 			boxplot_data.push(object);				
 		}
@@ -21,7 +21,7 @@ drawBoxPlot = function(data,svg) {
 		padding_h = 20
 			
 		var xScale = d3.scale.linear()
-						.domain([d3.min(boxplot_data, function(d) { return d[x_var];}),(d3.max(boxplot_data, function(d) { return d[x_var];}))])
+						.domain([d3.min(boxplot_data, function(d) { return d[xVar];}),(d3.max(boxplot_data, function(d) { return d[xVar];}))])
 						.range([padding_w, w-padding_w]);
 
 		var yScale = d3.scale.linear()
@@ -32,8 +32,8 @@ drawBoxPlot = function(data,svg) {
 			   .data(boxplot_data)
 			   .enter()
 			   .append("rect")
-			   .attr("x",function(d,i) {
-					return xScale(i);
+			   .attr("x",function(d) {
+					return xScale(xVar);
 					})			
 			   .attr("y",function(d){
 					return yScale(d.quartile_1);
@@ -48,14 +48,14 @@ drawBoxPlot = function(data,svg) {
 			   .data(boxplot_data)
 			   .enter()
 			   .append("line")
-			   .attr("x1",function(d,i) {
-					return xScale(i) + xScale(0.5)/2;
+			   .attr("x1",function(d) {
+					return xScale(xVar) + xScale(0.5)/2;
 					})			
 			   .attr("y1",function(d){
 					return yScale(d.minimum);
 				})			
 			   .attr("x2",function(d,i) {
-					return xScale(i) + xScale(0.5)/2;
+					return xScale(xVar) + xScale(0.5)/2;
 					})			
 			   .attr("y2",function(d){
 					return yScale(d.maximum);
@@ -65,14 +65,14 @@ drawBoxPlot = function(data,svg) {
 			   .data(boxplot_data)
 			   .enter()
 			   .append("line")
-			   .attr("x1",function(d,i) {
-					return xScale(i) + xScale(0.5)/3;
+			   .attr("x1",function(d) {
+					return xScale(xVar) + xScale(0.5)/3;
 					})			
 			   .attr("y1",function(d){
 					return yScale(d.minimum);
 				})			
 			   .attr("x2",function(d,i) {
-					return xScale(i) + xScale(0.5)*2/3;
+					return xScale(xVar) + xScale(0.5)*2/3;
 					})			
 			   .attr("y2",function(d){
 					return yScale(d.minimum);
